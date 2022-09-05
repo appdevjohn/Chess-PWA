805e3c880b2b4774809d59aca26fc9fbe26db2c7
John Champion <johnt.champion@gmail.com>
2022-09-05 11:53:00 -0400

    Make the new game transition more smooth.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,37 @@ import './App.css';
 
 
 function App() {
+    // The board width in pixels, as required by react-chessboard.
     const [boardWidth, setBoardWidth] = useState(window.visualViewport.width);
-    const [game, setGame] = useState(new Chess());
-    const [isResettingGame, setIsResettingGame] = useState(false);
-    const playerColor = useState(Math.random() > 0.5 ? 'w' : 'b')[0];
 
+    // The game state, in the chess.js format.
+    const [game, setGame] = useState(new Chess());
+
+    // To create the animation to a new game, we need to keep the old game state briefly.
+    const [oldGameFEN, setOldGameFEN] = useState();
+
+    // The player's color.
+    const playerColor = useState(Math.random() > 0.5 ? 'w' : 'b')[0];
     const playerColorFull = playerColor === 'w' ? 'white' : 'black';
 
-    window.addEventListener('resize', () => {
-        if (window.visualViewport.width < window.visualViewport.height) {
-            setBoardWidth(window.visualViewport.width);
-        } else {
-            setBoardWidth(window.visualViewport.height);
-        }
-    });
-
+    // Manages the width of the board in pixels.
     useEffect(() => {
-        if (isResettingGame) { return }
+        const resizeBoard = () => {
+            if (window.visualViewport.width < window.visualViewport.height) {
+                setBoardWidth(window.visualViewport.width);
+            } else {
+                setBoardWidth(window.visualViewport.height);
+            }
+        }
+
+        window.addEventListener('resize', resizeBoard);
+        return () => window.removeEventListener('resize', resizeBoard)
+
+    }, [])
+
+    // Manages what happens after each the game state changes.
+    useEffect(() => {
+        if (oldGameFEN) { return }
 
         if (game.turn() !== playerColor) {
             // Call API to get best possible move.
@@ -58,19 +72,18 @@ function App() {
             }
         }, 300)
 
-    }, [game, playerColor, playerColorFull, isResettingGame])
+    }, [game, playerColor, playerColorFull, oldGameFEN])
 
+    // Ends the animation of a game reset.
     useEffect(() => {
-        if (isResettingGame && game.history().length > 0) {
-            const gameCopy = { ...game };
-            gameCopy.reset();
-            setGame(gameCopy);
+        if (oldGameFEN) {
             setTimeout(() => {
-                setIsResettingGame(false);
-            }, 300)
+                setOldGameFEN(undefined);
+            }, 500)
         }
-    }, [game, isResettingGame])
+    }, [oldGameFEN])
 
+    // Make a move. Returns true if the move is valid.
     const makeMove = move => {
         const gameCopy = { ...game };
         const result = gameCopy.move(move);
@@ -78,6 +91,7 @@ function App() {
         return result;
     }
 
+    // Handles what happens when a piece is dropped on a square.
     const pieceDroppedHandler = (sourceSquare, targetSquare) => {
         const move = {
             from: sourceSquare,
@@ -88,40 +102,46 @@ function App() {
         return validMove !== null;
     }
 
+    // Handles what happens when a game reset is requested.
+    const resetGameHandler = () => {
+        setOldGameFEN(game.fen());
+        const gameCopy = { ...game };
+        gameCopy.reset();
+        setGame(gameCopy);
+    }
+
+    // Chessboard array. If resetting a game, a board with the resigned game
+    // will be added to the front of the array, and both boards will appear
+    // briefly on the screen.
+    const chessBoards = [
+        <Chessboard
+            id='chess-game-1'
+            boardOrientation={playerColorFull}
+            boardWidth={boardWidth}
+            position={game.fen()}
+            onPieceDrop={pieceDroppedHandler}
+            snapToCursor={false}
+            animationDuration={game.history().length === 0 ? 0 : 300}
+            key='main'
+        />
+    ]
+    if (oldGameFEN) {
+        chessBoards.unshift(
+            <Chessboard
+                boardOrientation={playerColorFull}
+                boardWidth={boardWidth}
+                position={oldGameFEN}
+                snapToCursor={false}
+                animationDuration={0}
+                key='animated'
+            />
+        )
+    }
+
     return (
         <div className="App">
-            <button style={{ position: 'absolute', zIndex: 2 }} onClick={() => setIsResettingGame(true)}>Reset</button>
-            {isResettingGame ? (
-                <div className='chessboard-container tossed-game'>
-                    <Chessboard
-                        id='chessboard'
-                        boardOrientation={playerColorFull}
-                        boardWidth={boardWidth}
-                        position={game.fen()}
-                        snapToCursor={false}
-                        animationDuration={0}
-                    />
-                    <Chessboard
-                        boardOrientation={playerColorFull}
-                        boardWidth={boardWidth}
-                        position={new Chess().fen()}
-                        snapToCursor={false}
-                        animationDuration={0}
-                    />
-                </div>
-            ) : (
-                <div className='chessboard-container'>
-                    <Chessboard
-                        id='chessboard'
-                        boardOrientation={playerColorFull}
-                        boardWidth={boardWidth}
-                        position={game.fen()}
-                        onPieceDrop={pieceDroppedHandler}
-                        snapToCursor={false}
-                        animationDuration={300}
-                    />
-                </div>
-            )}
+            <button style={{ position: 'absolute', zIndex: 2 }} onClick={resetGameHandler}>Reset</button>
+            <div className={oldGameFEN ? 'chessboard-container tossed-game' : 'chessboard-container'}>{chessBoards}</div>
         </div>
     );
 }
