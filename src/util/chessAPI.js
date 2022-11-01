5b172a4e867c0fe62d6ff79d3c7557a91a610adb
John Champion <johnt.champion@gmail.com>
2022-10-31 22:39:17 -0400

    Environment variable can be set for base URL path.

--- src/util/chessAPI.js
+++ src/util/chessAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const chessAPI = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.REACT_APP_CHESS_API_BASE_URL || 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json'
     }
